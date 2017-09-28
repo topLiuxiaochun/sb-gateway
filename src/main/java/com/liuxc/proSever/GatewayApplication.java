23357af4474f0da891bc01e0f094b12488323121
liuxc_gitlab <liu_xiaochun_ssh@163.com>
2017-09-28 16:03:43 +0800

    创建服务网关项目

---
+++ src/main/java/com/liuxc/proSever/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.liuxc.proSever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy			//
+@EnableCircuitBreaker		//
+@EnableDiscoveryClient		// 注册服务到注册中心
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/liuxc/proSever/controller/ConfigServerController.java
@@ -0,0 +1,19 @@
+//package com.liuxc.proSever.controller;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RefreshScope
+//@RestController
+//public class ConfigServerController {
+//
+//	@Value("${from}")
+//	private String from;
+//	
+//	@RequestMapping("/from")
+//	public String fromValue() {
+//		return from;
+//	}
+//}
